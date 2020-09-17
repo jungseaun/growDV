58204083498f7a794b15f0e779601cb1e92ae077
computer <computer@DESKTOP-4FBE4KS>
2020-09-17 22:47:53 +0900

    최초 GitHub 연동

---
+++ myTestFile/src/myTestFile/FlowExam00.java
@@ -0,0 +1,23 @@
+package myTestFile;
+
+import java.util.Scanner;
+
+public class FlowExam00 {
+
+	public static void main(String[] args) {
+		int menu = 0, num = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		outer:while(true) {
+			System.out.println("(1) square");
+			System.out.println("(2) square root");
+			System.out.println("(3) log");
+			System.out.println("원하는 메뉴");
+			break;
+		}
+		
+
+	}
+
+}
